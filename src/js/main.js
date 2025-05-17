3fc2fd5e0604ccacb1188f98acf1aacce1a9c567
rysmartinez <rysmartinez7@gmail.com>
2025-05-17 14:42:15 -0500

    Week02 Individual Activity: Dynamic Product List

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,8 @@
-// main.js
-//import ProductList from './ProductList.mjs';
-//import ProductData from './ProductData.mjs'; // <-- Import ProductData
+import ProductList from "./ProductList.mjs";
+import ProductData from "./ProductData.mjs";
 
-//const list = new ProductList('tents');
-//console.log(list.getProducts());
-
-import ProductList from './ProductList.mjs';
-import ProductData from './ProductData.mjs';
-
-const category = 'tents';
-const listElement = document.querySelector('#product-list'); // <ul id="product-list">
+const category = "tents";
+const listElement = document.querySelector("#product-list"); // <ul id="product-list">
 const dataSource = new ProductData(category);
 
 const productList = new ProductList(category, dataSource, listElement);
@@ -19,16 +12,14 @@ productList.init(); // this triggers the rendering
 //const dataSource = new ProductData('tents');
 
 // Example usage of getData() and findProductById
-dataSource.getData().then(products => {
-    console.log('All products:', products);
+dataSource.getData().then((products) => {
+  console.log("All products:", products);
 });
 
-dataSource.findProductById('12345').then(product => {
-    if (product) {
-        console.log('Found product:', product);
-    } else {
-        console.log('Product not found');
-    }
+dataSource.findProductById("12345").then((product) => {
+  if (product) {
+    console.log("Found product:", product);
+  } else {
+    console.log("Product not found");
+  }
 });
-
-
